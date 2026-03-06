82935eb991c03b77c3646230c850eb5221566504
michassm <michelm240703@gmail.com>
2026-03-06 11:40:02 -0300

    Verificador de variaveis par ou ímpar

---
+++ src/main/java/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int x , y;
+        x = 10;
+
+        if (x % 2 == 0){
+            System.out.println("x é par");
+        }else{
+            System.out.println("x é impar");
+        }
+
+        y = 17;
+        if (y % 2 == 0){
+            System.out.println("y é par");
+        }else{
+            System.out.println("y é impar");
+        }
+    }
+}
